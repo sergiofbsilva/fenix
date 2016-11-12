4cdb6d091b0889464af5e0ae68aa9ba5d292b604
Fernando Santos <fernandopinadossantos@gmail.com>
2016-11-12 23:34:24 +0000

    Allow Student Listings by Degree to be filtered by Program Conclusion ACDM-1143 #resolves

--- src/main/java/org/fenixedu/academic/ui/renderers/providers/choiceType/replacement/multiple/StudentStatuteTypeProvider.java
+++ src/main/java/org/fenixedu/academic/ui/renderers/providers/choiceType/replacement/multiple/StudentStatuteTypeProvider.java
@@ -23,10 +23,18 @@ import java.util.stream.Collectors;
 import org.fenixedu.academic.domain.student.StatuteType;
 import org.fenixedu.academic.ui.renderers.providers.AbstractDomainObjectProvider;
 
+import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyArrayConverter;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+
 public class StudentStatuteTypeProvider extends AbstractDomainObjectProvider {
 
     @Override
     public Object provide(Object source, Object currentValue) {
         return StatuteType.readAll().collect(Collectors.toList());
+    }
+    
+    @Override
+    public Converter getConverter() {
+        return new DomainObjectKeyArrayConverter();
     }
 }
